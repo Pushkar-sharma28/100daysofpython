d7b17f7b3391cfd5958ddbb776ec29606c87e9f9
Pushkar-sharma28 <pushkarsharma8800@gmail.com>
2022-11-24 20:43:20 +0530

    day 1

--- day001.py
+++ day001.py
@@ -1,3 +1,14 @@
 #taking input
 name = input("enter the name")
 print(len(name))
+#variables
+name ="pushkar"
+ruppe=10
+
+
+#day 1 project band name generator
+print("Yo\n")
+city=input("Enter the name of ur city")
+print(city)
+pet_name=input("what is the name of ur pet")
+print("Your band name can be "+city+" "+pet_name+"\n")
