eb26b01dc66ef2c7daed73d464528b05020521c8
Pushkar-sharma28 <pushkarsharma8800@gmail.com>
2022-11-28 00:05:20 +0530

    day006

---
+++ day006.py
@@ -0,0 +1,18 @@
+#functions : a piece of code that can be used again and again
+
+def func_name(variables):
+    #statements
+    print("1")
+    # return values
+    return "hello"
+print(func_name(1))
+
+#intendations we can do it with tabs as well as spaces
+#do not mix them
+
+
+#while loops
+#syntax
+while condition:
+    "statements once the condtion is fullifilled loop stops"
+
